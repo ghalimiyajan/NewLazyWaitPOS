e491858e4ee22e66f0967cc7c8e5c455f98141bf
feda14 <46413933+feda14@users.noreply.github.com>
2019-07-21 15:36:47 +0300

    Swiper + Edit login style

--- ojb.js
+++ ojb.js
@@ -1,21 +1,42 @@
 const noticeBoard = {
-    title:'Last update',
-    tag:'New!',
-    day:'5',
-    Month:'December',
-    VERSIONnUMBER:'V4.2.2',
-    details:[
-    {
-        title: 'Cashier Updates',
-        detail:'Ruqyay Alzayer ',
+    title: {
+        en: 'Last update',
+        ar: 'آخر التحديثات'
     },
-    {
-        title: 'Precurements Updats',
-        detail:'Bla bla bla bla Bla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla bla',
-
+    tag: {
+        en: 'New!',
+        ar: 'جديد!'
     },
-    
-]}
+    day: '5',
+    Month: {
+        en: 'December',
+        ar: 'ديسمبر'
+    },
+    VERSIONnUMBER: 'V4.2.2',
+    details: [
+        {
+            title: {
+                en: 'Cashier Updates',
+                ar: 'نظام الكاشير'
+            },
+            detail: {
+                en: 'Ruqyay Alzayer ',
+                ar: 'رقية الزاير'
+            },
+        },
+        {
+            title: { en: 'Precurements Updats', },
+            detail: {
+                en: 'Bla bla bla bla Bla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla blaBla bla bla bla',
+                ar: '   على نسبة المجموع المضاف إضافة خاصية تحديد التخفيض للمنتجات المختارة بناء '
+            },
+
+        },
+
+    ]
+}
 
 
-export default noticeBoard
+
+export default noticeBoard;
+
